260175a7d7ee64941f54ed09adab8279c64d3f28
Maxime Kohlhaas <mko@atm-consulting.fr>
2013-02-07 10:23:25 +0100

    Encodage : suppression accents pour compatibilité

--- class/export.class.php
+++ class/export.class.php
@@ -260,6 +260,7 @@ class ExportCompta {
 	function get_line(&$format, $dataline) {		
 		$ligneFichierTxtFixe = '';
 		foreach($format as $fmt) {
+			// Récupération valeur
 			$valeur = '';
 			if($fmt['type_value'] == 'data') {
 				$valeur = $dataline[$fmt['name']];
@@ -269,18 +270,48 @@ class ExportCompta {
 				$valeur = $fmt['value'];
 			}
 			
+			// Gestion du format de la valeur
 			if($valeur == '') $valeur = $fmt['default'];
 			if($fmt['type'] == 'date' && !empty($valeur) && !empty($fmt['format'])) $valeur = date($fmt['format'], $valeur);
-			if(mb_strlen($valeur, 'UTF-8') < $fmt['length']) {
+			
+			// Suppression de tous les caractères accentués pour compatibilités tous systèmes
+			$valeur = $this->suppr_accents($valeur);
+			
+			// Ajout padding ou troncature
+			if(strlen($valeur) < $fmt['length']) {
 				$pad_string = ($fmt['default'] == '') ? ' ' : $fmt['default'];
 				$pad_type = !empty($fmt['pad_type']) ? $fmt['pad_type'] : STR_PAD_LEFT;
-				$valeur = str_pad($valeur, $fmt['length']+(strlen($valeur)-mb_strlen($valeur)), $pad_string, $pad_type);
+				$valeur = str_pad($valeur, $fmt['length'], $pad_string, $pad_type);
 			} else if(mb_strlen($valeur,'UTF-8') > $fmt['length']) {
-				$valeur = mb_substr($valeur, 0, $fmt['length'],'UTF-8');
+				$valeur = substr($valeur, 0, $fmt['length']);
 			}
 			$ligneFichierTxtFixe .= $valeur;
 		}
 		return $ligneFichierTxtFixe;
+	}
+	
+	/**
+	 * Supprimer les accents
+	 * 
+	 * @param string $str chaîne de caractères avec caractères accentués
+	 * @param string $encoding encodage du texte (exemple : utf-8, ISO-8859-1 ...)
+	 */
+	function suppr_accents($str, $encoding='utf-8')
+	{
+		// transformer les caractères accentués en entités HTML
+		$str = htmlentities($str, ENT_NOQUOTES, $encoding);
+	
+		// remplacer les entités HTML pour avoir juste le premier caractères non accentués
+		// Exemple : "&ecute;" => "e", "&Ecute;" => "E", "Ã " => "a" ...
+		$str = preg_replace('#&([A-za-z])(?:acute|grave|cedil|circ|orn|ring|slash|th|tilde|uml);#', '\1', $str);
+	
+		// Remplacer les ligatures tel que : Œ, Æ ...
+		// Exemple "Å“" => "oe"
+		$str = preg_replace('#&([A-za-z]{2})(?:lig);#', '\1', $str);
+		// Supprimer tout le reste
+		$str = preg_replace('#&[^;]+;#', '', $str);
+	
+		return $str;
 	}
 }
 ?>
