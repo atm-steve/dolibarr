496ba435938159b1b799d6848ea3da29f13c4c74
maxime peyrot <maxime.peyrot@atm-consulting.fr>
2015-07-10 14:46:10 +0200

    NEW : Export des écritures d'achat au format Cegid

--- class/export_cegid.class.php
+++ class/export_cegid.class.php
@@ -17,9 +17,10 @@ class TExportComptaCegid extends TExportCompta {
 			array('name' => 'sens',					'length' => 1,	'default' => 'D',	'type' => 'text',),
 			array('name' => 'montant',				'length' => 20,	'default' => '0',	'type' => 'text',),
 			array('name' => 'libelle',				'length' => 35,	'default' => '',	'type' => 'text'),
-			array('name' => 'numero_piece',			'length' => 35,	'default' => '',	'type' => 'text'),
-			
+			array('name' => 'numero_piece',			'length' => 35,	'default' => '',	'type' => 'text')
 		);
+		
+		$this->_format_ecritures_comptables_achat = $this->_format_ecritures_comptables_vente;
 		
 		$this->lineSeparator = "\r\n";
 		$this->fieldSeparator = ';';
@@ -94,6 +95,96 @@ class TExportComptaCegid extends TExportCompta {
 				}
 			}
 			
+			$numEcriture++;
+		}
+
+		return $contenuFichier;
+	}
+
+	function get_file_ecritures_comptables_achats($format, $dt_deb, $dt_fin) {
+		global $conf;
+
+		if(empty($format)) $format = $this->_format_ecritures_comptables_achat;
+
+		$TabFactures = parent::get_factures_fournisseur($dt_deb, $dt_fin);
+		
+		$contenuFichier = '';
+
+		$numEcriture = 1;
+		$numLignes = 1;
+		
+		$type = 'M';
+		$codeJournal='AC';
+		
+		foreach ($TabFactures as $id_facture => $infosFacture) {
+			$tiers = &$infosFacture['tiers'];
+			$facture = &$infosFacture['facture'];
+			
+			if(!empty($infosFacture['entity'])) {
+				$entity = $infosFacture['entity'];
+				$tmp = explode(";", $entity['description']);
+				$codeCompteTiers = !empty($tmp[0]) ? $tmp[0] : '';
+				$codeAnalytique = !empty($tmp[1]) ? $tmp[1] : '';
+			}
+			
+			$label = $tiers['nom'];
+			$label.= (!empty($facture['ref_client']) ? ' - '.$facture['ref_client']:'');
+
+			// Lignes client
+			foreach($infosFacture['ligne_tiers'] as $code_compta => $montant) {
+				$ligneFichier = array(
+					'date_ecriture'			=> $facture['date'],
+					'code_journal'			=> $codeJournal,
+					'numero_compte'			=> '40100000',
+					'numero_compte_aux'		=> $code_compta,
+					'sens'					=> ($montant > 0 ? 'C' : 'D'),
+					'montant'				=> number_format(abs($montant),2,',',''),
+					'libelle'				=> $label,
+					'numero_piece'			=> $facture['ref']
+				);
+				
+				// Ecriture générale
+				$contenuFichier .= parent::get_line($format, $ligneFichier);
+				$numLignes++;
+			}
+			
+			// Lignes de produits
+			foreach($infosFacture['ligne_produit'] as $code_compta => $montant) {
+				$ligneFichier = array(
+					'date_ecriture'			=> $facture['date'],
+					'code_journal'			=> $codeJournal,
+					'numero_compte'			=> $code_compta,
+					'sens'					=> ($montant > 0 ? 'D' : 'C'),
+					'montant'				=> number_format(abs($montant),2,',',''),
+					'libelle'				=> $label,
+					'numero_piece'			=> $facture['ref']
+				);
+				
+				// Ecriture générale
+				$contenuFichier .= parent::get_line($format, $ligneFichier);
+				
+				// Ecriture analytique
+				$numLignes++;
+			}
+
+			// Lignes TVA
+			if(!empty($infosFacture['ligne_tva'])) {
+				foreach($infosFacture['ligne_tva'] as $code_compta => $montant) {
+						$ligneFichier = array(
+							'date_ecriture'					=> $facture['date'],
+							'code_journal'					=> $codeJournal,
+							'numero_compte'					=> $code_compta,
+							'sens'							=> ($montant > 0 ? 'D' : 'C'),
+							'montant'						=> number_format(abs($montant),2,',',''),
+							'libelle'						=> $label,
+							'numero_piece'					=> $facture['ref']
+						);
+					
+					// Ecriture générale
+					$contenuFichier .= parent::get_line($format, $ligneFichier);
+					$numLignes++;
+				}
+			}
 			$numEcriture++;
 		}
 
