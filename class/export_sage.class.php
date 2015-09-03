d137ec320f876e5021e79cb0792b12dc4ff3bd0a
Alexis Algoud <alexis@atm-consulting.fr>
2015-09-03 16:23:25 +0200

    libe

--- class/export_sage.class.php
+++ class/export_sage.class.php
@@ -135,7 +135,7 @@ class TExportComptaSage extends TExportCompta {
 					'numero_compte_general'			=> "40100000",
 					'numero_compte_tiers'			=> $code_compta,
 	
-					'libelle'						=> (!empty($facture['libelle']) ? $facture['libelle'] : $tiers['nom']),
+					'libelle'						=> (!empty($facture['libelle']) ? $tiers['nom'].' '.$facture['libelle'] : $tiers['nom']),
 					'mode_rglt'						=> $facture['mode_reglement'],
 					'date_echeance'					=> $facture['date_lim_reglement'],
 					'montant_debit'					=> ($facture['type'] == 2 || $montant < 0) ? abs($montant) : 0,
