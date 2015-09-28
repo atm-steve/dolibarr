2b5ee92a2fd59f55209cbbdf6bee345f7c949b0a
phf <phf@atm-consulting.fr>
2015-09-28 11:41:47 +0200

    FIX correction sur le fonctionnement des factures de situations avec multicompany

--- htdocs/compta/facture/class/facture.class.php
+++ htdocs/compta/facture/class/facture.class.php
@@ -4461,7 +4461,7 @@ class Facture extends CommonInvoice
 		$sql = 'SELECT rowid FROM '.MAIN_DB_PREFIX.'facture';
 		$sql .= ' WHERE situation_cycle_ref = '.$this->situation_cycle_ref;
 		$sql .= ' AND situation_counter < '.$this->situation_counter;
-		$sql .= ' AND entity = '.($this->entity > 0 ? $this->entity : $conf->entity);
+		$sql .= ' AND entity in ('.getEntity('facture').')';
 		$resql = $this->db->query($sql);
 		$res = array();
 		if ($resql && $resql->num_rows > 0) {
@@ -4542,8 +4542,8 @@ class Facture extends CommonInvoice
 		if (!empty($this->situation_cycle_ref)) {
 			// No point in testing anything if we're not inside a cycle
 			$sql = 'SELECT max(situation_counter) FROM '.MAIN_DB_PREFIX.'facture';
-			$sql .= ' WHERE situation_cycle_ref = '.$this->situation_cycle_ref;
-			$sql .= ' AND entity = '.($this->entity > 0 ? $this->entity : $conf->entity);
+			$sql .= ' WHERE situation_cycle_ref = '.(int) $this->situation_cycle_ref;
+			$sql .= ' AND entity in ('.getEntity('facture').')';
 			$resql = $this->db->query($sql);
 
 			if ($resql && $resql->num_rows > 0) {
