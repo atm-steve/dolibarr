92b0cc87d9d461921518427ad767827e08997c68
geoffrey.girard <geoffre.girard@atm-consulting.fr>
2015-10-13 15:34:47 +0200

    FIX

--- class/export_sage.class.php
+++ class/export_sage.class.php
@@ -429,11 +429,15 @@ class TExportComptaSage extends TExportCompta {
 		
 		foreach($TabTiers as $code_compta=>$tiers) {
 			
+			foreach($tiers as $key=>$value){
+				$tiers[$key] = strtr($value,array('amp;'=>'&','gt;'=>'>'));
+			}
+			
 			$ligneFichier=array_merge($tiers, array(
 				'numero_compte'=>$code_compta,
 				'numero_compte_general'	=> ($tiers['fournisseur']) ? "40100000" : "41100000",
 				'libelle'=>html_entity_decode($tiers['nom'],ENT_QUOTES,'UTF-8'),
-				'type_tiers'=>($tiers['fournisseur']) ? 2 : 1,
+				'type_tiers'=>($tiers['fournisseur']) ? 1 : 0,
 				'compte_collectif'=>$conf->global->COMPTA_ACCOUNT_CUSTOMER,
 				'adresse1'=>html_entity_decode($tiers['address'],ENT_QUOTES,'UTF-8'),
 				'zip'=>$tiers['zip'],
