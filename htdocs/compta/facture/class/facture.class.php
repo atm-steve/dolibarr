d2d4f0a677cea3457a0cc4df358b39ea2f9a2bfb
atm-ph <phf@atm-consulting.Fr>
2017-09-08 15:48:50 +0200

    Merge rebase 6.0

--- htdocs/compta/facture/class/facture.class.php
+++ htdocs/compta/facture/class/facture.class.php
@@ -1892,6 +1892,7 @@ class Facture extends CommonInvoice
 	            // Call trigger
 	            $result=$this->call_trigger('BILL_PAYED',$user);
 	            if ($result < 0) $error++;
+               
 	            // End call triggers
 			}
 			else
