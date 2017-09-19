dfe17a1bd69ae268fbb9c68126248a365ed506b9
atm-ph <phf@atm-consulting.Fr>
2017-09-19 17:33:19 +0200

    Fix reset du code car la requête SQL prend trop de temps et est fausse

--- htdocs/societe/class/societe.class.php
+++ htdocs/societe/class/societe.class.php
@@ -1695,13 +1695,14 @@ class Societe extends CommonObject
         {
         	$sql.= ", ".MAIN_DB_PREFIX."usergroup_user as ug";
         	$sql.= " WHERE ((ug.fk_user = sc.fk_user";
-        	$sql.= /*" AND ug.entity = ".$conf->entity.*/")";
+        	$sql.= " AND ug.entity = ".$conf->entity.")";
         	$sql.= " OR u.admin = 1)";
         }
         else
-        	//$sql.= " WHERE entity in (0, ".$conf->entity.")";
-			$sql.= " WHERE 1";
+        	$sql.= " WHERE entity in (0, ".$conf->entity.")";
 
+		$sql.= " AND u.rowid = sc.fk_user AND sc.fk_soc =".$this->id;
+		
         $resql = $this->db->query($sql);
         if ($resql)
         {
