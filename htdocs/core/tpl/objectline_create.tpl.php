e5b8e1c660a6a0e9091dbf62949154e90e320635
Marc de Lima Lucio <marc.delimalucio@atm-consulting.fr>
2017-11-10 12:26:28 +0100

    FIX: decimal rounding + ...

--- htdocs/core/tpl/objectline_create.tpl.php
+++ htdocs/core/tpl/objectline_create.tpl.php
@@ -444,9 +444,9 @@ if (! empty($usemargins) && $user->rights->margins->creer)
 		print "var dec='".$dec."'; var thousand='".$thousand."';\n";	// Set var in javascript
 		?>
 
-		var main_max_dec_shown = <?php echo $conf->global->MAIN_MAX_DECIMALS_SHOWN; ?>;
-		var main_rounding_unit = <?php echo $conf->global->MAIN_MAX_DECIMALS_UNIT; ?>;
-		var main_rounding_tot = <?php echo $conf->global->MAIN_MAX_DECIMALS_TOT; ?>;
+		var main_max_dec_shown = <?php echo (int) str_replace('.', '', $conf->global->MAIN_MAX_DECIMALS_SHOWN); ?>;
+		var main_rounding_unit = <?php echo (int) $conf->global->MAIN_MAX_DECIMALS_UNIT; ?>;
+		var main_rounding_tot = <?php echo (int) $conf->global->MAIN_MAX_DECIMALS_TOT; ?>;
 
 		var amount = num.toString();
 
