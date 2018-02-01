c776f4218dc81ac7a4cc9b682b503b534c5c9271
gauthier <gauthier.verdol@atm-consulting.fr>
2018-02-01 09:37:52 +0100

    FIX : getNomUrl() societe n'ouvre pas sytématiquement de nouvelle page

--- htdocs/societe/class/societe.class.php
+++ htdocs/societe/class/societe.class.php
@@ -1906,7 +1906,7 @@ class Societe extends CommonObject
         if (empty($linkstart))
         {
             $label.= '<u>' . $langs->trans("ShowCompany") . '</u>';
-            $linkstart = '<a target="_blank"  href="'.DOL_URL_ROOT.'/societe/card.php?socid='.$this->id;
+            $linkstart = '<a href="'.DOL_URL_ROOT.'/societe/card.php?socid='.$this->id;
         }
 
         if (! empty($this->name))
