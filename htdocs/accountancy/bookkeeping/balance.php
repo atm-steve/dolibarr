048e95d03dd5cb5b8f11e4eaf5d342ae6c83ff5c
atm-quentin <quentin.vial-gouteyron@atm-consulting.fr>
2018-11-06 15:25:58 +0100

    FIX 8658 export csv use ',' so number have to use '.'

--- htdocs/accountancy/bookkeeping/balance.php
+++ htdocs/accountancy/bookkeeping/balance.php
@@ -151,10 +151,10 @@ if ($action == 'export_csv') {
 	{
 		print length_accountg($line->numero_compte) . $sep;
 		print $object->get_compte_desc($line->numero_compte) . $sep;
-		print price($line->debit) . $sep;
-		print price($line->credit) . $sep;
-		print price($line->debit) . $sep;
-		print price($line->credit - $line->debit) . $sep;
+		print price2num($line->debit) . $sep;
+		print price2num($line->credit) . $sep;
+		print price2num($line->debit) . $sep;
+		print price2num($line->credit - $line->debit) . $sep;
 		print "\n";
 	}
 }
