8909b7d13a354f53180281c571c07fe78c21f5f4
ATM-Nicolas <nicolas.prevost@atm-consulting.fr>
2018-12-05 09:06:27 +0100

    FIX : Check invoice statut to see create credit note button

--- htdocs/compta/facture/card.php
+++ htdocs/compta/facture/card.php
@@ -5062,6 +5062,7 @@ else if ($id > 0 || ! empty($ref))
 
 			// For situation invoice with excess received
 			if ($object->statut > Facture::STATUS_DRAFT
+                && $object->type == Facture::TYPE_SITUATION
 			    && ($object->total_ttc - $totalpaye - $totalcreditnotes - $totaldeposits) > 0
 			    && $user->rights->facture->creer
 			    && !$objectidnext
