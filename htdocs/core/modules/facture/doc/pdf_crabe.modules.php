794dbcca4ac7069ad9430416abb972ba22475d68
atm-quentin <quentin.vial-gouteyron@atm-consulting.fr>
2019-04-29 14:50:47 +0200

    FIX PDF crabe wasnt on good folder

--- htdocs/core/modules/facture/doc/pdf_crabe.modules.php
+++ htdocs/core/modules/facture/doc/pdf_crabe.modules.php
@@ -202,6 +202,15 @@ class pdf_crabe extends ModelePDFFactures
 	{
 		global $user,$langs,$conf,$mysoc,$db,$hookmanager,$nblignes;
 
+        if($conf->entity != $object->entity) {
+            // Récupération configuration de l'entité de la facture
+
+            $conf->entity = $object->entity;
+            $conf->setValues($db);
+            $mysoc->setMysoc($conf);
+            $this->emetteur=$mysoc;
+        }
+
 		if (! is_object($outputlangs)) $outputlangs=$langs;
 		// For backward compatibility with FPDF, force output charset to ISO, because FPDF expect text to be encoded in ISO
 		if (! empty($conf->global->MAIN_USE_FPDF)) $outputlangs->charset_output='ISO-8859-1';
@@ -241,6 +250,9 @@ class pdf_crabe extends ModelePDFFactures
 
 		if(! empty($conf->global->MULTICOMPANY_INVOICE_SHARING_ENABLED)) $upload_dir = $conf->facture->multidir_output[$object->entity];
 		else $upload_dir = $conf->facture->dir_output;
+
+        if($object->entity == 1)$upload_dir= str_replace('invoice','facture',$upload_dir);
+        else $upload_dir= str_replace('facture','invoice',$upload_dir);
 
 		if ($upload_dir)
 		{
