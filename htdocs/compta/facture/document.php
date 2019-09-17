2bf63bc75e39b68af5ee508a695fdec0f64097c2
atm-ph <phf@atm-consulting.fr>
2019-09-17 11:51:45 +0200

    FIX  PH: tk10040 : badge nombre fichiers

--- htdocs/compta/facture/document.php
+++ htdocs/compta/facture/document.php
@@ -97,9 +97,11 @@ if ($id > 0 || ! empty($ref))
 	{
 		$object->fetch_thirdparty();
 
-		$upload_dir = $conf->facture->multidir_output[$object->entity].'/'.dol_sanitizeFileName($object->ref);
-        if($object->entity == 1)$upload_dir= str_replace('invoice','facture',$upload_dir);
-        else $upload_dir= str_replace('facture','invoice',$upload_dir);
+		#$upload_dir = $conf->facture->multidir_output[$object->entity].'/'.dol_sanitizeFileName($object->ref);
+        #if($object->entity == 1)$upload_dir= str_replace('invoice','facture',$upload_dir);
+        #else $upload_dir= str_replace('facture','invoice',$upload_dir);
+
+        $upload_dir = $conf->facture->dir_output.'/'.dol_sanitizeFileName($object->ref);
 
 		$head = facture_prepare_head($object);
 		dol_fiche_head($head, 'documents', $langs->trans('InvoiceCustomer'), -1, 'bill');
@@ -181,7 +183,7 @@ if ($id > 0 || ! empty($ref))
 		$permission = $user->rights->facture->creer;
 		$permtoedit = $user->rights->facture->creer;
 		$param = 'id=' . $object->id;
-		include_once DOL_DOCUMENT_ROOT . '/core/tpl/document_actions_post_headers.tpl.php';
+        include_once DOL_DOCUMENT_ROOT . '/core/tpl/document_actions_post_headers.tpl.php';
 	}
 	else
 	{
