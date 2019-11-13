d9cf54407ab695d1033e8fba10fa5cffe8e1c1a5
atm-greg <gregory.blemand@atm-consulting.fr>
2019-11-13 10:03:02 +0100

    fix space in prices during exportcsv

--- htdocs/accountancy/journal/sellsjournal.php
+++ htdocs/accountancy/journal/sellsjournal.php
@@ -595,8 +595,8 @@ if ($action == 'exportcsv') {		// ISO and not UTF8 !
 				print '"' . length_accounta(html_entity_decode($k)) . '"' . $sep;
 				print '"' . $langs->trans("Thirdparty") . '"' . $sep;
 				print '"' . utf8_decode(dol_trunc($companystatic->name, 16)) . ' - ' . $invoicestatic->ref . ' - ' . $langs->trans("Thirdparty") . '"' . $sep;
-				print '"' . ($mt >= 0 ? price($mt) : '') . '"' . $sep;
-				print '"' . ($mt < 0 ? price(- $mt) : '') . '"' . $sep;
+				print '"' . ($mt >= 0 ? price2num($mt) : '') . '"' . $sep;
+				print '"' . ($mt < 0 ? price2num(- $mt) : '') . '"' . $sep;
 				print '"' . $journal . '"';
 				print "\n";
 			//}
@@ -616,8 +616,8 @@ if ($action == 'exportcsv') {		// ISO and not UTF8 !
 				print '""' . $sep;
 				print '"' . utf8_decode(dol_trunc($accountingaccount->label, 32)) . '"' . $sep;
 				print '"' . utf8_decode(dol_trunc($companystatic->name, 16)) . ' - ' . dol_trunc($accountingaccount->label, 32) . '"' . $sep;
-				print '"' . ($mt < 0 ? price(- $mt) : '') . '"' . $sep;
-				print '"' . ($mt >= 0 ? price($mt) : '') . '"' . $sep;
+				print '"' . ($mt < 0 ? price2num(- $mt) : '') . '"' . $sep;
+				print '"' . ($mt >= 0 ? price2num($mt) : '') . '"' . $sep;
 				print '"' . $journal . '"';
 				print "\n";
 			//}
@@ -641,8 +641,8 @@ if ($action == 'exportcsv') {		// ISO and not UTF8 !
 					print '""' . $sep;
 					print '"' . $langs->trans("VAT") . ' - ' . $def_tva[$key] . ' %"' . $sep;
 					print '"' . utf8_decode(dol_trunc($companystatic->name, 16)) . ' - ' . $invoicestatic->ref . ' - ' . $langs->trans("VAT") . join(', ', $def_tva[$key][$k]) .' %' . ($numtax?' - Localtax '.$numtax:'') . '"' . $sep;
-					print '"' . ($mt < 0 ? price(- $mt) : '') . '"' . $sep;
-					print '"' . ($mt >= 0 ? price($mt) : '') . '"' . $sep;
+					print '"' . ($mt < 0 ? price2num(- $mt) : '') . '"' . $sep;
+					print '"' . ($mt >= 0 ? price2num($mt) : '') . '"' . $sep;
 					print '"' . $journal . '"';
 					print "\n";
 				}
