b45edec33f4a4d9a017f311d4f6fb891c424bb18
atm-lena <lena@atm-consulting.fr>
2020-02-10 16:24:36 +0100

    Spé ATM : date par défaut sur création expédition

--- htdocs/expedition/card.php
+++ htdocs/expedition/card.php
@@ -969,7 +969,7 @@ if ($action == 'create')
             print '<td colspan="3">';
             //print dol_print_date($object->date_livraison, "day");	// date_livraison come from order and will be stored into date_delivery planed.
             $date_delivery = ($date_delivery ? $date_delivery : $object->date_livraison); // $date_delivery comes from GETPOST
-            print $form->selectDate($date_delivery ? $date_delivery : -1, 'date_delivery', 1, 1, 1);
+            print $form->selectDate($date_delivery?$date_delivery:strtotime('today'), 'date_delivery', 1, 1, 1);
             print "</td>\n";
             print '</tr>';
 
