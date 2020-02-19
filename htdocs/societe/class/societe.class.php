abb23e32b1b844728df913a83d05e393e8c43b00
ATM-Nicolas <nicolas.prevost@atm-consulting.fr>
2020-02-19 09:34:22 +0100

    FIX : getSalesRepresentatives function, add entity 1 with transverse mode

--- htdocs/societe/class/societe.class.php
+++ htdocs/societe/class/societe.class.php
@@ -1866,7 +1866,7 @@ class Societe extends CommonObject
 		{
 			$sql.= ", ".MAIN_DB_PREFIX."usergroup_user as ug";
 			$sql.= " WHERE ((ug.fk_user = sc.fk_user";
-			$sql.= " AND ug.entity = ".$conf->entity.")";
+			$sql.= " AND ug.entity IN (1, ".$conf->entity."))";
 			$sql.= " OR u.admin = 1)";
 		}
 		else
