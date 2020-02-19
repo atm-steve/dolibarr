c5b0c7126f99b9217c8b9786941a0bb4b8e8f7e1
ATM-Nicolas <nicolas.prevost@atm-consulting.fr>
2020-02-19 08:58:08 +0100

    FIX : Afficher correctement les users dans les select_user

--- htdocs/core/class/html.form.class.php
+++ htdocs/core/class/html.form.class.php
@@ -1644,7 +1644,7 @@ class Form
 			{
 				$sql.= " LEFT JOIN ".MAIN_DB_PREFIX."usergroup_user as ug";
 				$sql.= " ON ug.fk_user = u.rowid";
-				$sql.= " WHERE ug.entity = ".$conf->entity;
+				$sql.= " WHERE ug.entity IN (1, ".$force_entity.')';
 			}
 			else
 			{
