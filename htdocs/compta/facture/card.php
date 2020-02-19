07398fe07aa0545e90bbf4aa05ad82c2d471acab
stickler-ci <support@stickler-ci.com>
2020-02-19 11:07:42 +0000

    Fixing style errors.

--- htdocs/compta/facture/card.php
+++ htdocs/compta/facture/card.php
@@ -3318,7 +3318,6 @@ if ($action == 'create')
 
 
 	if($conf->global->INVOICE_USE_RETAINED_WARRANTY){
-
 		$rwStyle = 'display:none;';
 		if(in_array(GETPOST('type', 'int'), $RetainedWarrantyInvoiceAvailableType)){
 			$rwStyle = '';
