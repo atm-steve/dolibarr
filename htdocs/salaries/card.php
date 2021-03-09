948fe48dc444ad354fff58c1d9a8cdbe76a982c3
x <x@x.x>
2021-03-09 11:50:18 +0100

    FIX : No fetch required

--- htdocs/salaries/card.php
+++ htdocs/salaries/card.php
@@ -640,11 +640,9 @@ if ($id)
 		$morehtmlref .= '</form>';
 	}
 
-	$sal = new Salary($db);
-	$sal->fetch($object->id);
 	//Employee
 	if($action != 'editfk_user') {
-		if ($sal->getSommePaiement() > 0 && !empty($object->fk_user)){
+		if ($object->getSommePaiement() > 0 && !empty($object->fk_user)){
 			$userstatic = new User($db);
 			$result = $userstatic->fetch($object->fk_user);
 			if ($result > 0){
