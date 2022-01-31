20ee10249ce4f708fd7167c1ab87243de32bfca9
Quentin VIAL-GOUTEYRON <quentin.vial-gouteyron@atm-consulting.fr>
2022-01-31 16:09:31 +0100

    NEW Exclude filter

--- htdocs/core/lib/functions.lib.php
+++ htdocs/core/lib/functions.lib.php
@@ -7997,17 +7997,24 @@ function natural_search($fields, $value, $mode = 0, $nofirstand = 0)
 
 					$newres .= (($i2 > 0 || $i3 > 0) ? ' OR ' : '');
 
-					if (preg_match('/\.(id|rowid)$/', $field))	// Special case for rowid that is sometimes a ref so used as a search field
+					if (preg_match('/^!/', $field))	// Special case for rowid that is sometimes a ref so used as a search field
 					{
 						$newres .= $field." = ".(is_numeric(trim($tmpcrit)) ?trim($tmpcrit) : '0');
 					}
 					else
 					{
-						$newres .= $field." LIKE '";
 
 						$tmpcrit = trim($tmpcrit);
 						$tmpcrit2 = $tmpcrit;
 						$tmpbefore = '%'; $tmpafter = '%';
+
+						if(preg_match('/^!/', $tmpcrit)) {
+							$newres .= $field." NOT LIKE '"; // ! as exclude character
+							$tmpcrit2 = preg_replace('/^!/', '', $tmpcrit2);
+						}
+						else $newres .= $field." LIKE '";
+
+
 						if (preg_match('/^[\^\$]/', $tmpcrit))
 						{
 							$tmpbefore = '';
