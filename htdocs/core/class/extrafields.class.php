b6e2f00c9808b6b225b33d2b0d7638f674b04033
Quentin VIAL-GOUTEYRON <quentin.vial-gouteyron@atm-consulting.fr>
2022-02-03 09:51:04 +0100

    add comment spec sgp

--- htdocs/core/class/extrafields.class.php
+++ htdocs/core/class/extrafields.class.php
@@ -1794,6 +1794,7 @@ class ExtraFields
 					$toprint[] = '<li class="select2-search-choice-dolibarr noborderoncategories" style="background: #aaa">'.$param['options'][$valueval].'</li>';
 				}
 			}
+			//overflow:auto;max-height:100px; spec sgp
 			$value = '<div class="select2-container-multi-dolibarr" style="width: 90%;overflow:auto;max-height:100px;"><ul class="select2-choices-dolibarr">'.implode(' ', $toprint).'</ul></div>';
 		}
 		elseif ($type == 'chkbxlst')
@@ -1859,6 +1860,7 @@ class ExtraFields
 						}
 					}
 				}
+				//overflow:auto;max-height:100px; spec sgp
 				$value = '<div class="select2-container-multi-dolibarr" style="width: 90%;overflow:auto;max-height:100px;"><ul class="select2-choices-dolibarr">'.implode(' ', $toprint).'</ul></div>';
 			} else {
 				dol_syslog(get_class($this).'::showOutputField error '.$this->db->lasterror(), LOG_WARNING);
